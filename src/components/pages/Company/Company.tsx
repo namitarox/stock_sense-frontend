a16a8cfdf2513ed6bb439fea5a2317bce3bacaa7
浪岡大貴 <namioka@pixel-hearts.co.jp>
2024-02-25 20:40:28 +0900

    update 企業ページの雛形を作成

---
+++ src/components/pages/Company/Company.tsx
@@ -0,0 +1,38 @@
+import React, { FC, useState, useEffect } from "react";
+import { useRouter } from "next/router"; // Next.jsのルーターフックをインポート
+import { Header } from "@/components/layouts/Header";
+import { fetchStockCode } from "@/utils/logics/stockCode";
+import { StockCode } from "@/types/stockCode";
+
+export const Company: FC = () => {
+  const router = useRouter();
+  const { id } = router.query; // URLからcodeパラメータを取得
+  console.log(id);
+  const [stockCode, setStockCode] = useState<StockCode | null>(null);
+  useEffect(() => {
+    if (typeof id === "string") {
+      // codeがstring型であることを確認
+      fetchStockCode(id)
+        .then(setStockCode)
+        .catch((error) => console.error("Fetching Stock code failed:", error));
+    }
+  }, [id]);
+
+  return (
+    <>
+      <Header />
+      <main>
+        {stockCode ? (
+          <div>
+            <h1>{stockCode.name}</h1>
+            <p>Code: {stockCode.id}</p>
+            <p>Market: {stockCode.market}</p>
+            <p>Category: {stockCode.category}</p>
+          </div>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </main>
+    </>
+  );
+};
---
+++ src/pages/company/[id]/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next';
+import { Company } from '@/components/pages/Company';
+
+const CompanyPage: NextPage = () => {
+  return <Company />;
+};
+
+export default CompanyPage;
